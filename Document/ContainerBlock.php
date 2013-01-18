634774091c8dfac65caaa2fbac6f2719fae2dd6d
Nicolas Bastien <nbastien@prestaconcept.net>
2013-01-18 15:03:34 +0100

    Add a addChild method to container block

--- Document/ContainerBlock.php
+++ Document/ContainerBlock.php
@@ -4,6 +4,8 @@ namespace Symfony\Cmf\Bundle\BlockBundle\Document;
 
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
 use Doctrine\ODM\PHPCR\ChildrenCollection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Sonata\BlockBundle\Model\BlockInterface;
 
 /**
  * Block that contains other blocks ...
@@ -28,5 +30,24 @@ class ContainerBlock extends BaseBlock
     public function setChildren(ChildrenCollection $children)
     {
         return $this->children = $children;
+    }
+
+    /**
+     * Add a child to this container
+     *
+     * @param  BlockInterface $child
+     * @param  string $key OPTIONAL
+     * @return boolean
+     */
+    public function addChild(BlockInterface $child, $key = null)
+    {
+        if (null === $this->children) {
+            $this->children = new ArrayCollection();
+        }
+        if ($key != null) {
+            return $this->children->set($key, $child);
+        }
+
+        return $this->children->add($child);
     }
 }
