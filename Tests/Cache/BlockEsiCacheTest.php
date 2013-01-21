645f80ba094f8a2ccb88157354c451e6eaac0e76
Roel Sint <roel@beeldspraak.com>
2013-01-21 22:56:47 +0100

    cache: add tests

---
+++ Tests/Cache/BlockEsiCacheTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\BlockBundle\Tests\Cache;
+
+use Symfony\Cmf\Bundle\BlockBundle\Cache\BlockEsiCache;
+
+class BlockEsiCacheTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \RuntimeException
+     * @dataProvider      getExceptionCacheKeys
+     */
+    public function testExceptions($keys)
+    {
+        $router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+
+        $blockRenderer = $this->getMock('Sonata\BlockBundle\Block\BlockRendererInterface');
+
+        $blockLoader = $this->getMock('Sonata\BlockBundle\Block\BlockLoaderInterface');
+
+        $cache = new BlockEsiCache('My Token', array(), $router, $blockRenderer, $blockLoader);
+
+        $cache->get($keys, 'data');
+    }
+
+    public static function getExceptionCacheKeys()
+    {
+        return array(
+            array(array()),
+            array(array('block_id' => '/cms/content/home/additionalInfoBlock')),
+            array(array('updated_at' => 'foo')),
+        );
+    }
+
+    public function testInitCache()
+    {
+        $router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+        $router->expects($this->any())->method('generate')->will($this->returnValue('http://cmf.symfony.com/symfony-cmf/block/cache/esi/XXX/%2Fcms%2Fcontent%2Fhome%2FadditionalInfoBlock?updated_at=as'));
+
+        $blockRenderer = $this->getMock('Sonata\BlockBundle\Block\BlockRendererInterface');
+
+        $blockLoader = $this->getMock('Sonata\BlockBundle\Block\BlockLoaderInterface');
+
+        $cache = new BlockEsiCache('My Token', array(), $router, $blockRenderer, $blockLoader);
+
+        $this->assertTrue($cache->flush(array()));
+        $this->assertTrue($cache->flushAll());
+
+        $keys = array(
+            'block_id'   => '/cms/content/home/additionalInfoBlock',
+            'updated_at' => 'as',
+        );
+
+        $cacheElement = $cache->set($keys, 'data');
+
+        $this->assertInstanceOf('Sonata\CacheBundle\Cache\CacheElement', $cacheElement);
+
+        $this->assertTrue($cache->has(array('id' => 7)));
+
+        $cacheElement = $cache->get($keys);
+
+        $this->assertInstanceOf('Sonata\CacheBundle\Cache\CacheElement', $cacheElement);
+
+        $this->assertEquals('<esi:include src="http://cmf.symfony.com/symfony-cmf/block/cache/esi/XXX/%2Fcms%2Fcontent%2Fhome%2FadditionalInfoBlock?updated_at=as" />', $cacheElement->getData()->getContent());
+    }
+
+}
---
+++ Tests/Cache/BlockJsCacheTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\BlockBundle\Tests\Cache;
+
+use Symfony\Cmf\Bundle\BlockBundle\Cache\BlockJsCache;
+
+/**
+ *
+ */
+class BlockJsCacheTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \RuntimeException
+     * @dataProvider getExceptionCacheKeys
+     */
+    public function testExceptions($keys)
+    {
+        $router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+
+        $blockRenderer = $this->getMock('Sonata\BlockBundle\Block\BlockRendererInterface');
+
+        $blockLoader = $this->getMock('Sonata\BlockBundle\Block\BlockLoaderInterface');
+
+        $cache = new BlockJsCache($router, $blockRenderer, false, $blockLoader);
+
+        $cache->get($keys, 'data');
+    }
+
+    public static function getExceptionCacheKeys()
+    {
+        return array(
+            array(array()),
+            array(array('block_id' => '/cms/content/home/additionalInfoBlock')),
+            array(array('updated_at' => 'foo')),
+        );
+    }
+
+    public function testInitCache()
+    {
+        $router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+        $router->expects($this->once())->method('generate')->will($this->returnValue('http://cmf.symfony.com/symfony-cmf/block/cache/js-async.js'));
+
+        $blockRenderer = $this->getMock('Sonata\BlockBundle\Block\BlockRendererInterface');
+
+        $blockLoader = $this->getMock('Sonata\BlockBundle\Block\BlockLoaderInterface');
+
+        $cache = new BlockJsCache($router, $blockRenderer, false, $blockLoader);
+
+
+        $this->assertTrue($cache->flush(array()));
+        $this->assertTrue($cache->flushAll());
+
+        $keys = array(
+            'block_id'   => '/cms/content/home/additionalInfoBlock',
+            'updated_at' => 'as',
+        );
+
+        $cacheElement = $cache->set($keys, 'data');
+
+        $this->assertInstanceOf('Sonata\CacheBundle\Cache\CacheElement', $cacheElement);
+
+        $this->assertTrue($cache->has(array('id' => 7)));
+
+        $cacheElement = $cache->get($keys);
+
+        $this->assertInstanceOf('Sonata\CacheBundle\Cache\CacheElement', $cacheElement);
+
+        $expected = <<<EXPECTED
+<div id="block-cms-content-home-additionalInfoBlock" >
+    <script type="text/javascript">
+        /*<![CDATA[*/
+
+            (function() {
+                var b = document.createElement('script');
+                b.type = 'text/javascript';
+                b.async = true;
+                b.src = 'http://cmf.symfony.com/symfony-cmf/block/cache/js-async.js'
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(b, s);
+            })();
+
+        /*]]>*/
+    </script>
+</div>
+EXPECTED;
+
+        $this->assertEquals($expected, $cacheElement->getData()->getContent());
+    }
+
+}
