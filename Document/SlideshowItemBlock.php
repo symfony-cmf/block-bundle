228becde56b307422ff44508b2e0e0a391d81faf
Alain Horner <alain.horner@liip.ch>
2013-02-11 17:04:15 +0100

    initial slideshowblock

---
+++ Document/SlideshowItemBlock.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\BlockBundle\Document;
+
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+use Symfony\Cmf\Bundle\BlockBundle\Document\BaseBlock;
+
+/**
+ * Block that acts as an item of a SlideshowBlock
+ *
+ * @PHPCRODM\Document(referenceable=true)
+ */
+class SlideshowItemBlock extends BaseBlock
+{
+
+    /**
+     * Image file child
+     *
+     * @PHPCRODM\Child(name="image", cascade="persist")
+     */
+    protected $image;
+
+    /** @PHPCRODM\String */
+    protected $label;
+
+    /** @PHPCRODM\Node */
+    protected $node;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return 'symfony_cmf.block.slideshow_item';
+    }
+
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getNode()
+    {
+        return $this->node;
+    }
+
+}
