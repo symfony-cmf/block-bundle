2c9ed210d890957b0c8ba54b2f32d1419ce389ff
Alain Horner <alain.horner@liip.ch>
2013-02-27 15:30:13 +0100

    added TODO, corrected name

--- Admin/SlideshowItemAdmin.php
+++ Admin/SlideshowItemAdmin.php
@@ -7,21 +7,19 @@ use Sonata\AdminBundle\Form\FormMapper;
 
 class SlideshowItemAdmin extends Admin
 {
-
     protected function configureFormFields(FormMapper $formMapper)
     {
         parent::configureFormFields($formMapper);
 
         // image is only required when creating a new item
+        // TODO: sonata is not using one admin instance per object, so this doesnt really work - fix it
         $imageRequired = ($this->getSubject() && $this->getSubject()->getParent()) ? false : true;
 
         $formMapper
             ->with('form.group_general')
                 ->add('label', 'text')
-                ->add('image', 'phpcr_image', array('required' => $imageRequired, 'label' => 'Slide Image', 'data_class' => 'Doctrine\ODM\PHPCR\Document\Image'))
+                ->add('image', 'phpcr_odm_image', array('required' => $imageRequired, 'label' => 'Slide Image', 'data_class' => 'Doctrine\ODM\PHPCR\Document\Image'))
                 ->add('position', 'hidden', array('mapped' => false))
             ->end();
-
     }
-
 }
