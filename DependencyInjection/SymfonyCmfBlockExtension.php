985d03c7d99fabd5e388b09550473272f9f68cca
WouterJ <waldio.webdesign@gmail.com>
2013-04-20 22:39:29 +0200

    Created XML schema

--- DependencyInjection/SymfonyCmfBlockExtension.php
+++ DependencyInjection/SymfonyCmfBlockExtension.php
@@ -137,6 +137,16 @@ class SymfonyCmfBlockExtension extends Extension
         }
     }
 
+    /**
+     * Returns the base path for the XSD files.
+     *
+     * @return string The XSD base path
+     */
+    public function getXsdValidationBasePath()
+    {
+        return __DIR__.'/../Resources/config/schema';
+    }
+
     public function getNamespace()
     {
         return 'http://cmf.symfony.com/schema/dic/block';
