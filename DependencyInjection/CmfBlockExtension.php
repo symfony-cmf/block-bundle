7d9b9982489220f4c6cafcdd9543d9aa0b2e8467
Roel Sint <roel@beeldspraak.com>
2013-06-22 16:04:05 +0200

    Implement PrependExtensionInterface

--- DependencyInjection/CmfBlockExtension.php
+++ DependencyInjection/CmfBlockExtension.php
@@ -1,14 +1,31 @@
 <?php
 namespace Symfony\Cmf\Bundle\BlockBundle\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class CmfBlockExtension extends Extension
+class CmfBlockExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        // get all Bundles
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (isset($bundles['SonataBlockBundle'])) {
+            $config = array('templates' => array(
+                'block_base' => 'CmfBlockBundle:Block:block_base.html.twig'
+            ));
+            $container->prependExtensionConfig('sonata_block', $config);
+        }
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $config = $this->processConfiguration(new Configuration(), $configs);
