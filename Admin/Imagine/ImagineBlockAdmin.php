e1e33e7f907053837bef46353a2c8829993f1e5b
Nicolas Bastien <nbastien@prestaconcept.net>
2013-08-07 12:26:36 +0200

    #52 : Remove file header as it's not clearly set

--- Admin/Imagine/ImagineBlockAdmin.php
+++ Admin/Imagine/ImagineBlockAdmin.php
@@ -1,12 +1,5 @@
 <?php
-/*
- * This file is part of the Symfony CMF package.
- *
- * (c) Symfony2 CMF
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 namespace Symfony\Cmf\Bundle\BlockBundle\Admin\Imagine;
 
 use Symfony\Cmf\Bundle\BlockBundle\Admin\BaseBlockAdmin;
