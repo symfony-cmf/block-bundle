f94fd208ad965245554defe9322258e996317d63
WouterJ <waldio.webdesign@gmail.com>
2015-10-27 14:33:03 +0100

    Fix Symfony 2.8 deprecations

--- Admin/ActionBlockAdmin.php
+++ Admin/ActionBlockAdmin.php
@@ -37,11 +37,15 @@ class ActionBlockAdmin extends AbstractBlockAdmin
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $isSf28 = method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix');
+        $doctrineTreeType = $isSf28 ? 'Sonata\DoctrinePHPCRAdminBundle\Form\Type\TreeModelType' : 'doctrine_phpcr_odm_tree';
+        $textType = $isSf28 ? 'Symfony\Component\Form\Extension\Core\Type\TextType' : 'text';
+
         $formMapper
             ->with('form.group_general')
-            ->add('parentDocument', 'doctrine_phpcr_odm_tree', array('root_node' => $this->getRootPath(), 'choice_list' => array(), 'select_root_node' => true))
-            ->add('name', 'text')
-            ->add('actionName', 'text')
+            ->add('parentDocument', $doctrineTreeType, array('root_node' => $this->getRootPath(), 'choice_list' => array(), 'select_root_node' => true))
+            ->add('name', $textType)
+            ->add('actionName', $textType)
             ->end()
         ;
     }
