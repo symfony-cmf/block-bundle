f7719c084c2363b2d69cc9f0908c456e2c47c060
ElectricMaxxx <maximilian.berghoff@gmx.de>
2019-02-04 09:33:59 +0100

    root node defintion for SF < 4.2 also

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -32,7 +32,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('cmf_block');
-        $rootNode = $treeBuilder->getRootNode();
+        // Keep compatibility with symfony/config < 4.2
+        if (!method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->root('cmf_block');
+        } else {
+            $rootNode = $treeBuilder->getRootNode();
+        }
 
         $rootNode
             ->children()
